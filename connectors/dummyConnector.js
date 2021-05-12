f58c9d02b5caa7974d147432832f0ed53d83dd89
Ming Ying <yingming18@gmail.com>
2021-05-12 10:24:56 -0400

    Change fake URL to a fixed dummy real URL.

--- connectors/dummyConnector.js
+++ connectors/dummyConnector.js
@@ -1,3 +1,5 @@
 module.exports = async function (file_id) {
-  return `https://fake_domain.com/fake_path/${file_id}`;
+  console.log(`File id '${file_id}' requested, dummy (fixed) url will be returned.`);
+  return `http://www.africau.edu/images/default/sample.pdf`;
+  // return `https://fake_domain.com/fake_path/${file_id}`;
 }
